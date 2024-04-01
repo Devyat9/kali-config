efa33c706a88630a3291159764c7bff797d1c464
Devyat-XIII <69820277+Devyat-XIII@users.noreply.github.com>
2024-04-01 10:43:24 -0300

    Update entorno_config.sh

--- entorno_config.sh
+++ entorno_config.sh
@@ -30,7 +30,7 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zi
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip
 unzip Iosevka.zip -d ~/.local/share/fonts/
 unzip RobotoMono.zip -d ~/.local/share/fonts/
-fc-cache -f
+fc-cache -fv
 
 
 # Alacritty
